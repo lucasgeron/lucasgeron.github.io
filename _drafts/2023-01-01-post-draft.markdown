b19ee5312daca63d0e764d7b62649da5bb92232f
Lucas Fernando <lucasf.geron@icloud.com>
2023-10-05 14:39:13 -0300

    feat/ page hit counter on posts

---
+++ _drafts/2023-01-01-post-draft.markdown
@@ -0,0 +1,32 @@
+<!-- ALERT -->
+  > An example showing the `tip` type prompt.
+  {: .prompt-tip }
+
+  > An example showing the `info` type prompt.
+  {: .prompt-info }
+
+  > An example showing the `warning` type prompt.
+  {: .prompt-warning }
+
+  > An example showing the `danger` type prompt.
+  {: .prompt-danger }
+
+
+  <!-- CODE -->
+  ```ruby
+  def foo
+    puts 'foo'
+  end
+  ```
+  {: file='file.rb' .nolineno}
+
+  <!-- IMG -->
+  ![](){: }
+
+  <!-- VÍDEO -->
+  {% include youtube.html id='' %}
+
+  <!-- FOOTNOTE -->
+  This is a Footnote[^1]
+
+  [^1]: This is a Footnote.
--- post.sh
+++ post.sh
@@ -37,9 +37,10 @@ toc: true
 github_repo:
 
 categories:
-# - Portifolio
-# - Tutorial
 # - Article
+# - Portfolio
+# - Product
+# - Tutorial
 
 tags:
 # Tech Tags
@@ -56,46 +57,11 @@ tags:
 ---
 
 # $TITLE
+
 $SHORT_DESCRIPTION
-
-   <!-- ALERT -->
-  > An example showing the `tip` type prompt.
-  {: .prompt-tip }
-
-  > An example showing the `info` type prompt.
-  {: .prompt-info }
-
-  > An example showing the `warning` type prompt.
-  {: .prompt-warning }
-
-  > An example showing the `danger` type prompt.
-  {: .prompt-danger }
-
-
-  <!-- CODE -->
-  ```ruby
-  def foo
-    puts 'foo'
-  end
-  ```
-  {: file='file.rb' .nolineno}
-
-  <!-- IMG -->
-  ![](){: }
-
-  <!-- VÍDEO -->
-  {% include youtube.html id='' %}
-
-  <!-- FOOTNOTE -->
-  This is a Footnote[^1]
-
-  [^1]: This is a Footnote.
 
   
   <!-- GITHUB REPO -->
   {%- include github.markdown -%}
-
-
-
 EOL
 
